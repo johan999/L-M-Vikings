daaa344e18f94c61ab5d084b78833a5110f51938
johan999 <johel964@student.liu.se>
2017-09-08 11:13:56 +0200

    Added NPC dialogue

---
+++ Legends & Myths. Vikings/Assets/UI/ActivateTextAtLine.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTextAtLine : MonoBehaviour {
+
+
+    public TextAsset theText;
+
+    public int startLine;
+    public int endLine;
+
+    public TextBoxManager theTextBox;
+
+    public bool destroyWhenActivated;
+
+    public bool requireButtonPress;
+    private bool waitForPress;
+
+	// Use this for initialization
+	void Start () {
+        theTextBox = FindObjectOfType<TextBoxManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (waitForPress && Input.GetKeyDown(KeyCode.J))
+        {
+            waitForPress = false;
+            theTextBox.ReloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.endAtLine = endLine;
+            theTextBox.EnableTextBox();
+
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            if (requireButtonPress)
+            {
+                waitForPress = true;
+                return;
+            }
+            theTextBox.ReloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.endAtLine = endLine;
+            theTextBox.EnableTextBox();
+
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.name == "Player")
+        {
+            waitForPress = false;
+        }
+    }
+}
